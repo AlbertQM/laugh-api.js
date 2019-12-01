8ca3833f7f990d67316da1be26eaaa9b4ca89875
AlbertQM <king4spam@ymail.com>
2019-12-01 19:46:01 +0000

    Add TypeScript to project

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+// Media Element containing the A/V feed
+const video = document.getElementById("video") as HTMLMediaElement;
+const predictionEl = document.getElementById("prediction");
+
+function startVideo() {
+  if (!video) {
+    return;
+  }
+
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("../../ext/face-api.js/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("../../ext/face-api.js/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("../../ext/face-api.js/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("../../ext/face-api.js/models")
+]).then(startVideo);
+
+document.onload = () => {
+  if (video && predictionEl) {
+    video.addEventListener("play", () => {
+      setInterval(async () => {
+        const detections = await faceapi
+          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks()
+          .withFaceExpressions();
+        const { expressions } = detections[0];
+        const bestGuess = Object.keys(expressions).reduce((a, b) =>
+          expressions[a] > expressions[b] ? a : b
+        );
+        predictionEl.innerHTML = bestGuess;
+      }, 100);
+    });
+  }
+};
