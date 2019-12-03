5d5e76cee79ddf518310c1f0a94d3632154a22c6
AlbertQM <king4spam@ymail.com>
2019-12-03 19:46:10 +0000

    WIP: Installed webpack and webpack-serve, created a npm module and more.

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
 import * as faceapi from "../node_modules/face-api.js";
 
-// Media Element containing the A/V feed
-const video = document.getElementById("video") as HTMLMediaElement;
+const MODELS_PATH = "./models";
+
+// Media Element containing the A/V feed.
+const video = document.getElementById("video") as HTMLVideoElement;
 const predictionEl = document.getElementById("prediction");
 
-function startVideo() {
-  if (!video) {
-    return;
-  }
-
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
-  );
-}
-
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("../../ext/face-api.js/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("../../ext/face-api.js/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("../../ext/face-api.js/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("../../ext/face-api.js/models")
-]).then(startVideo);
-
-document.onload = () => {
+function init() {
   if (video && predictionEl) {
     video.addEventListener("play", () => {
       setInterval(async () => {
@@ -33,10 +16,32 @@ document.onload = () => {
           .withFaceExpressions();
         const { expressions } = detections[0];
         const bestGuess = Object.keys(expressions).reduce((a, b) =>
+          // @ts-ignore
           expressions[a] > expressions[b] ? a : b
         );
         predictionEl.innerHTML = bestGuess;
       }, 100);
     });
   }
-};
+}
+
+function startVideo() {
+  if (!video) {
+    return;
+  }
+
+  init();
+
+  navigator.getUserMedia(
+    { video: {} },
+    stream => (video.srcObject = stream),
+    err => console.error(err)
+  );
+}
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri(MODELS_PATH),
+  faceapi.nets.faceLandmark68Net.loadFromUri(MODELS_PATH),
+  faceapi.nets.faceRecognitionNet.loadFromUri(MODELS_PATH),
+  faceapi.nets.faceExpressionNet.loadFromUri(MODELS_PATH)
+]).then(startVideo);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackPluginServe: Serve } = require("webpack-plugin-serve");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  context: __dirname,
   mode: "development",
-  entry: "./src/index.ts",
+  entry: ["./src/index.ts", "webpack-plugin-serve/client"],
   module: {
     rules: [
       {
@@ -18,5 +22,34 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+
+  plugins: [
+    // Clean up the dist folder
+    new CleanWebpackPlugin(),
+
+    // Copy static folder to build folder
+    new CopyWebpackPlugin([
+      {
+        from: "./src/static/**/*",
+        to: path.resolve(__dirname, "dist"),
+        flatten: true
+      },
+      {
+        from: "./ext/face-api.js/models/*",
+        to: path.resolve(__dirname, "dist/models"),
+        flatten: true
+      }
+    ]),
+
+    // Serve static files for dev
+    new Serve({
+      static: [path.resolve(__dirname, "dist/")]
+    })
+  ],
+
+  watch: true,
+
+  // Simplify debugging by having actual filenames in console errors
+  devtool: "inline-source-map"
 };
